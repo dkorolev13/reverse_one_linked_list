d4795cf8729ebbbd12c9be9351335da9d284b2fe
Dmitry Korolev <dkorolev13@gmail.com>
2023-01-19 12:25:54 +0300

    Update main.cpp
    Some refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -3,161 +3,122 @@
 
 using namespace std;
 
-
 struct Node {
-
-    char data;
-    
+    char data; 
     Node* next;
-
-    Node(char data) {
     
+    Node (char data) {
         this->data = data;
-        
         this->next = NULL;
     }
 };
 
-
 struct OneLinkedList {
-
     Node* head;
-
     Node* tail;
 
     OneLinkedList() {
-    
         this->head = this->tail = NULL;
     }
 
     ~OneLinkedList() {
-    
         while (head != NULL) pop_front();
     }
 
     void pop_front() {
-
-        if (head == NULL) return;
-
+        if (head == NULL)
+            return;
+        
         if (head == tail) {
-
             delete tail;
-
             head = tail = NULL;
-
             return;
         }
 
         Node* node = head;
-
         head = node->next;
-
         delete node;
     }
 
     void pop_back() {
-
-        if (tail == NULL) return;
+        if (tail == NULL)
+            return;
 
         if (head == tail) {
-
             delete tail;
-
             head = tail = NULL;
-
             return;
         }
 
         Node* node = head;
-
         for(; node->next != tail; node = node->next);
-
         node->next = NULL;
-
         delete tail;
-
         tail = node;
     }
 
     void push_back(char data) {
-    
         Node* node = new Node(data);
-        
-        if (head == NULL) head = node;
-        
-        if (tail != NULL) tail->next = node;
+        if (head == NULL)
+            head = node;
+      
+        if (tail != NULL)
+            tail->next = node;
         
         tail = node;
     }
 
     void print() {
-    
         Node* temp = head;
-        
+     
         while (temp != NULL) {
-        
             cout << temp->data << "-->";
-            
             temp = temp->next;
         }
     }
 
     void reverse() {
-    
         Node* current = head;
-        
         Node *prev = NULL, *next = NULL;
 
         while (current != NULL) {
-            
             next = current->next;
-           
             current->next = prev;
-            
             prev = current;
-            
             current = next;
          }
-
+        
         head = prev;
     }
 };
 
-
 int main(int argc, char *argv[])
 {
     OneLinkedList ll;
-
     string data;
-
+    
     cout << "Input data without \"spaces\": " << endl;
     
     getline(cin, data);
-
-    for (int i : data) {
     
+    for (int i : data)
         ll.push_back(i);
-    }
 
     cout << "Input linked list:\n";
     
     ll.print();
-
     ll.pop_front();
-
     ll.pop_back();
-
+    
     cout << "\nList after removing extreme elements:\n";
-
-    ll.print();
-
-    cout << "\nReversed linked list:\n";
-
-    ll.reverse();
-
+    
     ll.print();
     
+    cout << "\nReversed linked list:\n";
+    
+    ll.reverse();
+    ll.print();
     cout << "\n";
-
+    
     return 0;
 }
